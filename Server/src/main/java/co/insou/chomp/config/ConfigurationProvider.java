f3bbb9fc2816e45ec794c168f1a9550687ca7dc8
insou22 <zac.kologlu@gmail.com>
2017-11-17 14:07:03 +1100

    Default config file

--- Server/src/main/java/co/insou/chomp/config/ConfigurationProvider.java
+++ Server/src/main/java/co/insou/chomp/config/ConfigurationProvider.java
@@ -1,10 +1,13 @@
 package co.insou.chomp.config;
 
+import java.io.File;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 
 import com.google.gson.stream.JsonReader;
 import com.google.inject.Provider;
 
+import co.insou.chomp.Chomp;
 import co.insou.chomp.bean.Beans;
 import co.insou.chomp.gson.GsonProvider;
 import co.insou.chomp.util.except.Try;
@@ -19,7 +22,16 @@ class ConfigurationProvider implements Provider<ChompConfiguration> {
 
 	private JsonReader readConfigFile()
 	{
-		return new JsonReader(Try.to(() -> new FileReader("config.json")));
+		File file = new File("config.json");
+
+		if (file.exists())
+		{
+			return new JsonReader(Try.to(() -> new FileReader(file)));
+		}
+
+		Chomp.print("WARNING: config.json not found, defaulting to jar packaged configuration");
+
+		return new JsonReader(new InputStreamReader(Chomp.class.getResourceAsStream("/config.json")));
 	}
 
 }
